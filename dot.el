fe870a38a2914dff768c2f8b3d776d07050fc46b
Song <wenhao.sng@gmail.com>
2013-05-17 14:18:26 +0800

    fix a bug

--- dot.el
+++ dot.el
@@ -49,8 +49,8 @@
       '(("t" "Todo" entry (file+headline "~/Projects/todo/capture.org" "Tasks")
 	 "* TODO %?\n %i\n %a")))
 
-(setq org-tag-alist '(("academic" . ?a) ("potential" . ?p) ("work" . ?w)
-		      ("routinue" . ?r) ("entertainment" . ?p)
+(setq org-tag-alist '(("academic" . ?a) ("potential" . ?p) ("misc" . ?m)
+		      ("routinue" . ?r) ("entertainment" . ?e)
 		      ("fun" . ?f)))
 
 (setq org-clock-persist t)
